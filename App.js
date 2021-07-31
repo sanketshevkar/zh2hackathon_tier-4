1602dabf8cb64442af0ddb4e4c648b1ec8b6999b
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-08-01 04:03:22 +0530

    profile

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ function HomeScreen() {
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View>
       <Profile />
     </View>
   );
