ea3b7f44741c4d17fd49ead9cda00d6e6c957541
Gatij Taranekar <gatij.taranekar@gmail.com>
2021-08-13 03:36:30 +0530

    (feat): added marketplace screen

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import { View } from 'react-native';
 import Pin from './pages/Pin';
 import Name from './pages/Name';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AntDesign } from '@expo/vector-icons';
+import MarketPlace from './pages/MarketPlace'
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -24,12 +26,20 @@ function HomeScreen() {
   );
 }
 
-function SettingsScreen({mobileNumber}) {
+function SettingsScreen({ mobileNumber }) {
   return (
     <View style={{ backgroundColor: "#ffffff" }}>
-      <Profile mobileNumber={mobileNumber}/>
+      <Profile mobileNumber={mobileNumber} />
     </View>
   );
+}
+
+function MarketPlaceCollection({ mobileNumber }) {
+  return (
+    <View style={{ backgroundColor: "#ffffff" }}>
+      <MarketPlace mobileNumber={mobileNumber} />
+    </View>
+  )
 }
 
 export default function App() {
@@ -39,32 +49,32 @@ export default function App() {
   const [mobileNumber, setMobileNumber] = React.useState('9890010090');
   const [passcode, setpasscode] = React.useState('');
   const [login, setLogin] = React.useState(false);
-  React.useEffect(()=>{
+  React.useEffect(() => {
     const getData = async () => {
       try {
         const value = await AsyncStorage.getItem('@storage_Key')
-        if(value !== null) {
+        if (value !== null) {
           setLogin(true);
         }
-      } catch(e) {
+      } catch (e) {
         // error reading value
       }
     }
     getData();
   }, [])
-  if(!auth && !login) {
-    return(
+  if (!auth && !login) {
+    return (
       <NativeBaseProvider>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Name">
-          <Stack.Screen name="Name">
-              {props => <Name {...props} setFirstName={setFirstName} setLastName={setLastName}/>}
+            <Stack.Screen name="Name">
+              {props => <Name {...props} setFirstName={setFirstName} setLastName={setLastName} />}
             </Stack.Screen>
             <Stack.Screen name="OnBoardingPage">
               {props => <OnBoardingPage {...props} setMobileNumber={setMobileNumber} />}
             </Stack.Screen>
             <Stack.Screen name="Pin">
-              {props => <Pin {...props} auth={auth} setAuth={setAuth} setpasscode={setpasscode} firstName={firstName} lastName={lastName} mobileNumber={mobileNumber} passcode={passcode}/>}
+              {props => <Pin {...props} auth={auth} setAuth={setAuth} setpasscode={setpasscode} firstName={firstName} lastName={lastName} mobileNumber={mobileNumber} passcode={passcode} />}
             </Stack.Screen>
             {/* <Stack.Screen name="OnBoardingStatus" component={()=><OnBoardingStatus setAuth={setAuth}/>} /> */}
             <Stack.Screen name="OnBoardingStatus">
@@ -74,13 +84,13 @@ export default function App() {
         </NavigationContainer>
       </NativeBaseProvider>
     )
-  } else if(login && !auth) {
-    return(
+  } else if (login && !auth) {
+    return (
       <NativeBaseProvider>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Name">
             <Stack.Screen name="Pin">
-              {props => <Pin {...props} auth={auth} login={login} setAuth={setAuth} setpasscode={setpasscode} passcode={passcode}/>}
+              {props => <Pin {...props} auth={auth} login={login} setAuth={setAuth} setpasscode={setpasscode} passcode={passcode} />}
             </Stack.Screen>
             {/* <Stack.Screen name="OnBoardingStatus" component={()=><OnBoardingStatus setAuth={setAuth}/>} /> */}
             {/* <Stack.Screen name="OnBoardingStatus">
@@ -91,37 +101,44 @@ export default function App() {
       </NativeBaseProvider>
     )
   } else {
-    return(
+    return (
       <NativeBaseProvider>
-      <NavigationContainer>
-        <Tab.Navigator screenOptions={{
-          activeTintColor: '#e91e63',
-          activeBackgroundColor: "#ffffff",
-          inactiveBackgroundColor: "#ffffff"
-        }}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            if (route.name === 'Feed') {
-              return (
-                <MaterialIcons name="dynamic-feed" size={24} color="black" />
-              );
-            } else if (route.name === 'Profile') {
-              return (
-                <MaterialIcons name="home" size={24} color="black" />
-              );
-            }
-          },
-          tabBarInactiveTintColor: 'gray',
-          tabBarActiveTintColor: 'tomato',
-        })}
-        >
-          <Tab.Screen name="Feed" component={HomeScreen} />
-          <Tab.Screen name="Profile">
-              {props => <SettingsScreen {...props} mobileNumber={mobileNumber}/>}
-          </Tab.Screen>
-        </Tab.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={{
+            activeTintColor: '#e91e63',
+            activeBackgroundColor: "#ffffff",
+            inactiveBackgroundColor: "#ffffff"
+          }}
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                if (route.name === 'Feed') {
+                  return (
+                    <MaterialIcons name="dynamic-feed" size={24} color="black" />
+                  );
+                } else if (route.name === 'Profile') {
+                  return (
+                    <MaterialIcons name="home" size={24} color="black" />
+                  );
+                } else if (route.name === "Marketplace") {
+                  return (
+                    <AntDesign name="shoppingcart" size={24} color="black" />
+                  )
+                }
+              },
+              tabBarInactiveTintColor: 'gray',
+              tabBarActiveTintColor: 'tomato',
+            })}
+          >
+            <Tab.Screen name="Feed" component={HomeScreen} />
+            <Tab.Screen name="Marketplace">
+              {props => <MarketPlaceCollection {...props} mobileNumber={mobileNumber} />}
+            </Tab.Screen>
+            <Tab.Screen name="Profile">
+              {props => <SettingsScreen {...props} mobileNumber={mobileNumber} />}
+            </Tab.Screen>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
     )
   }
 }
